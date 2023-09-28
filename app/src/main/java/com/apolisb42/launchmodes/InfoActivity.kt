d61c48f147d545f9c8d253ba8d712cb654ce2b1d
Prasad Pawtekar <prasadpawtekra@gmail.com>
2023-09-29 02:08:00 +0530

    Single Task demo

--- app/src/main/java/com/apolisb42/launchmodes/InfoActivity.kt
+++ app/src/main/java/com/apolisb42/launchmodes/InfoActivity.kt
@@ -2,10 +2,16 @@ package com.apolisb42.launchmodes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class InfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LaunchModes", "onDestroy: InfoActivity")
     }
 }
